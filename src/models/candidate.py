8bfddbfda10e5f5475988832326ca5696cc86570
Gopalam Sameera Dimple <sameeradimple2001@gmail.com>
2025-08-30 13:02:26 +0530

    Added logic for qdrant vectordb and stuent registration forma and fixed minor bugs

--- src/models/candidate.py
+++ src/models/candidate.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, Field
 class Candidate(BaseModel):
     university_registration_number: str = Field(..., min_length=1)
     name: str = Field(..., min_length=1)
-    university_name: str = Field(..., min_length=1)
-    location: str = Field(..., min_length=1)
     semester: str = Field(..., min_length=1)
     branch: str = Field(..., min_length=1)
+    skills: list[str] = Field(default_factory=list)
